3938db48af204ce23171f5748ece597e9f910d3b
NguyenQuangAnh5598 <quanganh050598@gmail.com>
2021-10-17 11:51:31 +0700

    created studentTranscript class

--- src/manage/StudentManage.java
+++ src/manage/StudentManage.java
@@ -1,7 +1,18 @@
 package manage;
 
+import model.Transcript;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentManage {
-    public static void checkscrore() {
+    private static List<Transcript> transcriptList = new ArrayList<>();
+    public static void checkscrore(String studentId) {
+        for (int i = 0; i < transcriptList.size() ; i++) {
+            if (studentId.equals(transcriptList.get(i).getIdOfStudent())) {
+                System.out.println(transcriptList.get(i).getMath());
+            }
+        }
 
     }
 }
