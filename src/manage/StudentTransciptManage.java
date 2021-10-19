8092804d3e2e543bcfafaa5b8339416bf5edd676
NguyenQuangAnh5598 <quanganh050598@gmail.com>
2021-10-19 15:27:28 +0700

    complete basic method in StudentTranscriptManage class

--- src/manage/StudentTransciptManage.java
+++ src/manage/StudentTransciptManage.java
@@ -7,12 +7,12 @@ import model.Subjects;
 
 import java.util.List;
 
-public class StudenTransciptManage {
+public class StudentTransciptManage {
     private List<StudenTranscipt> studenTransciptList;
 
     public int getStudentIndex(String id) {
         int index = 0;
-        for (int i = 0; i < studenTransciptList.size()  ; i++) {
+        for (int i = 0; i < studenTransciptList.size(); i++) {
             if (id.equals(studenTransciptList.get(i).getStudent().getId())) {
                 index = i;
             }
@@ -22,7 +22,7 @@ public class StudenTransciptManage {
 
     public int getSubjectIndex(List<SubjectGeneric<Subjects, Integer>> studentSubjectList, String id) {
         int index = 0;
-        for (int i = 0; i < studentSubjectList.size()  ; i++) {
+        for (int i = 0; i < studentSubjectList.size(); i++) {
             if (id.equals(studentSubjectList.get(i).getSubject().getSubjectName())) {
                 index = i;
             }
@@ -30,27 +30,40 @@ public class StudenTransciptManage {
         return index;
     }
 
-    public void setScores(List<SubjectGeneric<Subjects, Integer>> studentSubjectList,String subjectName,int scores) {
-        studentSubjectList.get(getSubjectIndex(studentSubjectList,subjectName)).setScores(scores);
+
+    public void addStudent(Student student, List<SubjectGeneric<Subjects, Integer>> subjectGenericList) {
+        studenTransciptList.add(new StudenTranscipt(student, subjectGenericList));
     }
-    public void addStudent(Student student,List<SubjectGeneric<Subjects,Integer>> subjectGenericList) {
-        studenTransciptList.add(new StudenTranscipt(student,subjectGenericList));
+
+    public void setScores(List<SubjectGeneric<Subjects, Integer>> subjectList, String subjectName, int scores) {
+        subjectList.get(getSubjectIndex(subjectList, subjectName)).setScores(scores);
     }
-    public StudenTranscipt getStudentInfo(String id) {
-        for (int i = 0; i < studenTransciptList.size(); i++) {
-            if (id.equals(studenTransciptList.get(i).getStudent().getId())) {
-            return studenTransciptList.get(i);
-            }
-        }
-        return null;
+
+    public void setStudentName(String studentName, String id) {
+        studenTransciptList.get(getStudentIndex(id)).getStudent().setName(studentName);
     }
+
     public StudenTranscipt deleteStudent(String id) {
         studenTransciptList.remove(getStudentIndex(id));
         return studenTransciptList.get(getStudentIndex(id));
     }
-    public SubjectGeneric<Subjects, Integer> deleteSubject(List<SubjectGeneric<Subjects, Integer>> studentSubjectList, String subjectName) {
-        studentSubjectList.remove(getSubjectIndex(studentSubjectList,subjectName));
-        return studentSubjectList.get(getSubjectIndex(studentSubjectList,subjectName));
+
+    public StudenTranscipt getStudentInfo(String id) {
+        for (int i = 0; i < studenTransciptList.size(); i++) {
+            if (id.equals(studenTransciptList.get(i).getStudent().getId())) {
+                return studenTransciptList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void addNewSubject(List<SubjectGeneric<Subjects, Integer>> subjectList, Subjects subjects, int scores) {
+        subjectList.add(new SubjectGeneric<>(subjects, scores));
+    }
+
+    public SubjectGeneric<Subjects, Integer> deleteSubject(List<SubjectGeneric<Subjects, Integer>> subjectList, String subjectName) {
+        subjectList.remove(getSubjectIndex(subjectList, subjectName));
+        return subjectList.get(getSubjectIndex(subjectList, subjectName));
     }
 
 
